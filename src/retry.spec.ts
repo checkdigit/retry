af5af78bb7da05792a59fdf483875f997b0ec3a0
Carl Ansley <carl.ansley@gmail.com>
2023-04-20 16:22:59 -0400

    Update dependencies, add CodeQL and publish-beta actions (#9)
    * Update dependencies, add CodeQL and publish-beta actions
    
    * Update actions, formatting
    
    * --ignore-scripts

--- src/retry.spec.ts
+++ src/retry.spec.ts
@@ -1,12 +1,12 @@
 // retry.spec.ts
 
 /*
- * Copyright (c) 2021-2022 Check Digit, LLC
+ * Copyright (c) 2021-2023 Check Digit, LLC
  *
  * This code is licensed under the MIT license (see LICENSE.txt for details).
  */
 
-import assert from 'node:assert';
+import { strict as assert } from 'node:assert';
 
 import retry, { RetryError } from './index';
 
