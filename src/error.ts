89dd16e7b7773fbecd6221ce03174a421f2eafcd
Carl Ansley <carl.ansley@gmail.com>
2023-07-10 14:42:22 -0400

    Update comments/versioning

--- src/error.ts
+++ src/error.ts
@@ -1,5 +1,11 @@
 // error.ts
 
+/*
+ * Copyright (c) 2021-2023 Check Digit, LLC
+ *
+ * This code is licensed under the MIT license (see LICENSE.txt for details).
+ */
+
 export class RetryError extends Error {
   constructor(
     public retries: number,
