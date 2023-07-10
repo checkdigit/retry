589991ff6dae141a741d11a758eaa773a6c2af10
Carl Ansley <carl.ansley@gmail.com>
2023-07-10 15:11:24 -0400

    Switch to tag and release publishing (#13)

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,16 @@
 // error.ts
 
+/*
+ * Copyright (c) 2021-2023 Check Digit, LLC
+ *
+ * This code is licensed under the MIT license (see LICENSE.txt for details).
+ */
+
 export class RetryError extends Error {
-  constructor(public retries: number, public lastError: Error) {
+  constructor(
+    public retries: number,
+    public lastError: Error,
+  ) {
     super(`Maximum retries (${retries}) exceeded`);
   }
 }
